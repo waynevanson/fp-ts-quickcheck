5bc882b27b4580da141bb5e25ccf4163b91b0b12
Wayne Van Son <waynevanson@gmail.com>
2021-10-02 19:18:11 +1000

    test: fix bad comma

--- src/quickcheck.spec.ts
+++ src/quickcheck.spec.ts
@@ -4,13 +4,13 @@ import { pipe } from "fp-ts/lib/function"
 
 describe("qc", () => {
   it("shoudl work baby", () => {
-      pipe(
-        A.struct({ name: A.string, age: A.number }),
-        qc.run(({ name, age }) => true, {
-          count: 10,
-          initialSeed: 434,
-          size: 10,
-        }),
-      ),
+    pipe(
+      A.struct({ name: A.string, age: A.number }),
+      qc.run(({ name, age }) => true, {
+        count: 10,
+        initialSeed: 434,
+        size: 10,
+      }),
+    )
   })
 })
