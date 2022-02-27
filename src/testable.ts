b0ce8805695e0acc62eb1d94f4dff9acf8cfe0ac
Wayne Van Son <waynevanson@gmail.com>
2022-02-28 10:45:28 +1100

    feat: add testable assertion to catch most types

--- src/testable.ts
+++ src/testable.ts
@@ -5,6 +5,8 @@ import {
   io as IO,
   ioEither as IOE,
   option as O,
+  task as T,
+  taskEither as TE,
 } from "fp-ts"
 import { HKT, Kind, URIS } from "fp-ts/HKT"
 import { pipe } from "fp-ts/lib/function"
@@ -49,6 +51,49 @@ export const assertionAsync: Testable1<T.URI, Promise<void>> = {
     pipe(
       value,
       TE.tryCatchK(property, (e) => e),
+      TE.match(O.some, O.zero),
+    ),
+}
+export type Thunk<A> = () => A
+export type Promisable<A> = A | Promise<A>
+export type Thunkable<A> = A | Thunk<A>
+
+type Main = Thunkable<Promisable<boolean | void>>
+
+const fromMain =
+  <I>(property: (i: I) => Main) =>
+  (i: I): TE.TaskEither<unknown, boolean | void> =>
+  () => {
+    const main = property(i)
+    try {
+      let promisable: Promisable<boolean | void>
+
+      typeof main === "function" ? (promisable = main()) : (promisable = main)
+
+      return promisable instanceof Promise
+        ? promisable.then(E.right).catch(E.left)
+        : Promise.resolve(E.right(promisable))
+    } catch (e) {
+      return Promise.resolve(E.left(e))
+    }
+  }
+
+export const assertion: Testable1<T.URI, Main> = {
+  test: (value) => (property) =>
+    pipe(
+      fromMain(property)(value),
+      TE.chainEitherKW(
+        E.fromPredicate(
+          (a) => typeof a !== "boolean" || a,
+          () =>
+            new AssertionError({
+              operator: "boolean",
+              message: "Received false but expected true",
+              actual: false,
+              expected: true,
+            }),
+        ),
+      ),
       TE.match(O.some, O.zero),
     ),
 }
