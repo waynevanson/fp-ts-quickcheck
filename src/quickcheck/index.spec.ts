c86b33d06318c0da4757839cb734541039be9e25
Wayne Van Son <waynevanson@gmail.com>
2022-03-01 08:15:43 +1100

    tests: add some for assert

--- src/quickcheck/index.spec.ts
+++ src/quickcheck/index.spec.ts
@@ -1,4 +1,4 @@
-import { assertIO } from "./index"
+import { assertIO, assert } from "./index"
 import * as A from "../arbitrary"
 import { pipe } from "fp-ts/lib/function"
 
@@ -8,6 +8,16 @@ describe("assert", () => {
     pipe(
       A.tuple(A.number, A.number),
       assertIO(([x, y]) => expect(x + y).toBe(y + x)),
+    ),
+  )
+})
+
+describe("assert", () => {
+  it(
+    "should look functionalish",
+    pipe(
+      A.tuple(A.number, A.number),
+      assert(([x, y]) => Promise.resolve(true), { count: 10000 }),
     ),
   )
 })
