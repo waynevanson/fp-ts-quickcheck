68c2c4e3c40dedd03d539c63dbd649adaf88783c
Wayne Van Son <waynevanson@gmail.com>
2022-03-12 19:36:27 +1100

    chore: remove unused imports

--- src/modules/generators.ts
+++ src/modules/generators.ts
@@ -1,6 +1,6 @@
 import * as gen from "@no-day/fp-ts-generators"
 import * as lcg from "@no-day/fp-ts-lcg"
-import { state as S, monoid as M, number as Number } from "fp-ts"
+import { state as S } from "fp-ts"
 import { chainFirst as _chainFirst } from "fp-ts/lib/Chain"
 import { pipe } from "fp-ts/lib/function"
 import * as lens from "monocle-ts/Lens"
