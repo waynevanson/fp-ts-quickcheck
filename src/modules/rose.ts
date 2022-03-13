857298188bae7091cd7805c03203a09db8fbca30
Wayne Van Son <waynevanson@gmail.com>
2022-03-13 11:03:20 +1100

    refactor: remove rose module

--- src/modules/rose.ts
+++
@@ -1,9 +0,0 @@
-export interface Rose<A> {
-  readonly leaf: A
-  readonly roses: ReadonlyArray<Rose<A>>
-}
-
-// export const map: <A, B>(f: (a: A) => B) => (fa: Rose<A>) => Rose<B> =
-//   (f) => (fa) => {
-
-//   }
