ce9e8e9129d816e9235b58e327cb5dfc71fcb1f0
Wayne Van Son <waynevanson@gmail.com>
2022-03-22 06:37:33 +1100

    docs: add docs for quickcheck

--- src/quickcheck/index.ts
+++ src/quickcheck/index.ts
@@ -1,3 +1,12 @@
+/**
+ * @summary
+ * Quickcheck is a combinator library which can be used to compose generators 
+ * for property based tests.
+
+ * The `*assert*` functions run these generators as tests, safely lifting the
+ * property (not a key in an object, but a property in the context of property
+ * base testing) result from it's value `HKT<F, A>` to an `FromIO` instance.
+ */
 import { io as IO } from "fp-ts"
 import { flow } from "fp-ts/lib/function"
 import { task as T } from "../modules/fp-ts"
