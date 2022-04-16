cadc0439d70488a9970e1542b3c4825d40687aed
Wayne Van Son <waynevanson@gmail.com>
2022-04-16 20:31:55 +1000

    tests(arbitrary): add int.shrink

--- src/arbitrary.spec.ts
+++ src/arbitrary.spec.ts
@@ -1,5 +1,7 @@
 import { constVoid, pipe } from "fp-ts/lib/function"
 import * as AR from "./arbitrary"
+import { generator, iterable } from "./modules"
+import { state } from "./modules/fp-ts"
 import * as qc from "./quickcheck"
 
 describe("arbitrary", () => {
@@ -35,6 +37,36 @@ describe("arbitrary", () => {
       const y = AR.lazy(() => x)
       const x = AR.of(constVoid())
       expect(qc.io(y, constVoid)).not.toThrow()
+    })
+  })
+
+  describe("int", () => {
+    describe("shrink", () => {
+      it("should contain positive numbers only when given positive numbers", () => {
+        const number = AR.int({ min: 0 })
+        const size = AR.int()
+        const newSeed = pipe(AR.int(), AR.map(generator.mkSeed))
+
+        const genState = AR.struct({
+          newSeed,
+          size,
+        })
+
+        const arbitrary = AR.struct({
+          genState,
+          number,
+        })
+
+        qc.sync(arbitrary, ({ genState, number }) => {
+          const results = pipe(
+            AR.int({ min: number, max: number }).shrink,
+            state.evaluate(genState),
+            iterable.every((i: number) => i >= 0),
+          )
+
+          expect(results).toBeTruthy()
+        })
+      })
     })
   })
 })
