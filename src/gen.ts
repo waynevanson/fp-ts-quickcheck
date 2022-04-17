f7661507fcd762af9e8d56dfab1a8b6e37f90776
Wayne Van Son <waynevanson@gmail.com>
2022-04-17 16:24:04 +1000

    feat(gen): add `lazy`

--- src/gen.ts
+++ src/gen.ts
@@ -2,7 +2,7 @@ import * as gen from "@no-day/fp-ts-generators"
 import * as lcg from "@no-day/fp-ts-lcg"
 import { nonEmptyArray, state as S } from "fp-ts"
 import { chainFirst as _chainFirst } from "fp-ts/lib/Chain"
-import { pipe } from "fp-ts/lib/function"
+import { Lazy, pipe } from "fp-ts/lib/function"
 import * as lens from "monocle-ts/Lens"
 
 export * from "@no-day/fp-ts-generators"
@@ -38,3 +38,7 @@ export const map: <A, B>(f: (a: A) => B) => (fa: gen.Gen<A>) => gen.Gen<B> =
 export const ap: <A>(
   fa: gen.Gen<A>,
 ) => <B>(fab: gen.Gen<(a: A) => B>) => gen.Gen<B> = gen.ap
+
+export function lazy<A>(lazy: Lazy<gen.Gen<A>>): gen.Gen<A> {
+  return (s) => lazy()(s)
+}
