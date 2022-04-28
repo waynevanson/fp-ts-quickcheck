7cd06931f59af77d12ac29acc3fbcfeb99721491
Wayne Van Son <waynevanson@gmail.com>
2022-04-28 12:14:47 +1000

    chore(jest): add coverage configuration

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,11 @@ export default {
   preset: "ts-jest",
   testEnvironment: "node",
   watchPlugins: ["jest-runner-eslint/watch-fix"],
+  collectCoverage: true,
+  // coverageThreshold: {
+  //   global: { branches: 100, functions: 100, lines: 100, statements: 100 },
+  // },
+  roots: ["src"],
   projects: [
     {
       preset: "ts-jest",
