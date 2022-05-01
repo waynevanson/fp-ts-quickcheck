eb6ece84f8201d41dcfaf295880b2de0e86741eb
Wayne Van Son <waynevanson@gmail.com>
2022-05-01 10:32:02 +1000

    revert(coarbitrary): remove `coarbitrary` and its exports
    Can't think of a good use case for this, so let's remove it until there is some demand for it.
    
    BREAKING CHANGE: `coarbitrary` is no longer exported from the thing.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * as arbitrary from "./arbitrary"
-export * as coarbitrary from "./coarbitrary"
 export * as quickcheck from "./quickcheck"
 export * as gen from "./gen"
 export * as shrink from "./shrink"
