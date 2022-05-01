65621cd1db430af8bb0cc592ad95cdcf2db3c2da
Wayne Van Son <waynevanson@gmail.com>
2022-05-01 18:36:26 +1000

    feat: e

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,32 @@
+import {
+  readonlyArray,
+  io,
+  console,
+  readonlyNonEmptyArray,
+  option,
+  string,
+} from "fp-ts/lib/index.js"
+import { pipe, flow, constant } from "fp-ts/lib/function.js"
+import { default as micromatch } from "micromatch"
+
+export default (allFiles) =>
+  pipe(
+    micromatch(allFiles, "*.ts"),
+    readonlyNonEmptyArray.fromArray,
+    option.map((tsfiles) =>
+      pipe(
+        [
+          "eslint --cache --fix",
+          "prettier --write",
+          "jest --selectProjects unit --passWithNoTests",
+        ],
+        readonlyArray.map((string) => string.split(" ")),
+        readonlyArray.map(readonlyArray.alt(constant(tsfiles))),
+        readonlyArray.map(readonlyArray.intersperse(" ")),
+        readonlyArray.map(readonlyArray.foldMap(string.Monoid)((a) => a)),
+        readonlyArray.append("pnpm run docs"),
+        readonlyArray.append("git add ."),
+      ),
+    ),
+    option.getOrElse(readonlyArray.zero),
+  )
