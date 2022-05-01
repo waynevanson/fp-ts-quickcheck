f807778f354bca0c493007412fd9de39d0e5237a
Wayne Van Son <waynevanson@gmail.com>
2022-05-01 11:17:31 +1000

    docs(quickcheck): remove docs from `quickcheck` entrypoint

--- src/quickcheck/index.ts
+++ src/quickcheck/index.ts
@@ -1,11 +1,1 @@
-/**
- * @summary
- * Quickcheck is a combinator library which can be used to compose generators 
- * for property based tests.
-
- * The `*assert*` functions run these generators as tests, safely lifting the
- * property (not a key in an object, but a property in the context of property
- * base testing) result from it's value `HKT<F, A>` to an `FromIO` instance.
- */
-
 export { sync, async, io, task, quickcheckOptionsDefault, mk } from "./assert"
